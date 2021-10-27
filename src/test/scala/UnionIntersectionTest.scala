227391b8d17d57db2f0d7ff4bb5934d726407c00
Pappu Bhardwaj <47633505+codwin2506@users.noreply.github.com>
2021-10-27 08:54:34 +0530

    Update union Intersection Test

--- src/test/scala/UnionIntersectionTest.scala
+++ src/test/scala/UnionIntersectionTest.scala
@@ -5,7 +5,7 @@ class UnionIntersectionTest extends AnyFunSuite {
     val set1:Set[Int]=Set(1,2,3,4,5)
     val set2:Set[Int]=Set(1,6,3,7,7)
     val test1: UnionIntersection = new UnionIntersection()
-    val test2: UnionIntersection = new UnionIntersection()
+
     test("Intersection of two sets ") {
       assert(test1.theIntersection(set1,set2) == Set(1,3))
       assert(test1.theUnion(set1,set2) == Set(1,2,3,4,5,6,7))
@@ -13,8 +13,8 @@ class UnionIntersectionTest extends AnyFunSuite {
 
   val set3:Set[Int]=Set(1,25,3,40,5)
   val set4:Set[Int]=Set(10,6,3,75,7)
-
+  val test2: UnionIntersection = new UnionIntersection()
   test("Union of two sets ") {
-    assert(test1.theUnion(set1,set2) == Set(1,2,3,4,5,6,7))
+    assert(test2.theUnion(set3,set4) == Set(1,3,5,6,7,10,25,40,75))
   }
 }
